a872331d42a07f34c48df7e66ae443d1a9b35baf
uday1508 <udayg256@gmail.com>
2024-03-25 21:55:34 +0530

    notihing

--- Backend/ip.js
+++ Backend/ip.js
@@ -1,4 +1,4 @@
-const url = "http://192.168.93.73:8000/";
-const ipAddress = "192.168.93.73";
+const url = "http://192.168.22.73:8000/";
+const ipAddress = "192.168.22.73";
 module.exports = {ipAddress,url};
 
